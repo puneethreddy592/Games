5949682c877217c4f99fe9fb6ed8597d491d158b
Puneeth Reddy <145245941+puneethreddy592@users.noreply.github.com>
2024-03-28 03:16:54 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,6 @@
 #include "c42p.h"
 #include "tic_tac_toe_single.h"
 #include <stdio.h>
-//***************To make a master command********************
-// gcc -c library1.c -o library1.o
-// ar rcs liblibrary1.a library1.o
-// mv rock_paper_scissor.a librock_paper_scissor.a
-// gcc main.c -o main -L. -lrock_paper_scissor -ltic_tac_toe_2p -ltic_tac_toe_single -lc42p
 
 int main() {
     c42p();
